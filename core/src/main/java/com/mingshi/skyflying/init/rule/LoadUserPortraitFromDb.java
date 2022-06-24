c3bac2241b0236cf63543cd2868c896f6d80e63f
zhaoming <yiming2008@126.com>
2022-06-24 17:25:56 +0800

    更新前端代码

--- core/src/main/java/com/mingshi/skyflying/init/rule/LoadUserPortraitFromDb.java
+++ core/src/main/java/com/mingshi/skyflying/init/rule/LoadUserPortraitFromDb.java
@@ -3,10 +3,11 @@ package com.mingshi.skyflying.init;
 import com.mingshi.skyflying.anomaly_detection.singleton.AnomylyDetectionSingletonByVisitedTableEveryday;
 import com.mingshi.skyflying.anomaly_detection.singleton.AnomylyDetectionSingletonByVisitedTime;
 import com.mingshi.skyflying.dao.UserPortraitByVisitedTableEverydayMapper;
-import com.mingshi.skyflying.dao.UserPortraitByVisitedTableMapper;
 import com.mingshi.skyflying.dao.UserPortraitByVisitedTimeMapper;
+import com.mingshi.skyflying.dao.UserPortraitRulesMapper;
 import com.mingshi.skyflying.domain.UserPortraitByVisitedTableEverydayDo;
 import com.mingshi.skyflying.domain.UserPortraitByVisitedTimeDo;
+import com.mingshi.skyflying.domain.UserPortraitRulesDo;
 import com.mingshi.skyflying.enums.ConstantsCode;
 import com.mingshi.skyflying.utils.DateTimeUtil;
 import com.mingshi.skyflying.utils.StringUtil;
@@ -38,7 +39,7 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
   @Resource
   private UserPortraitByVisitedTimeMapper userPortraitByVisitedTimeMapper;
   @Resource
-  private UserPortraitByVisitedTableMapper userPortraitByVisitedTableMapper;
+  private UserPortraitRulesMapper userPortraitRulesMapper;
   @Resource
   private UserPortraitByVisitedTableEverydayMapper userPortraitByVisitedTableEverydayMapper;
 
@@ -55,8 +56,24 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
    **/
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    initUserPortraitByVisitedTimeMap();
-    initUserPortraitByVisitedTableEverydayMap();
+    // 从规则表中加载有哪些规则在使用，然后把具体的一条条详细规则加载到内存里来；2022-06-23 15:54:50
+    getAllUserPortraitRules();
+  }
+
+  private void getAllUserPortraitRules() {
+    List<UserPortraitRulesDo> userPortraitRulesDoList = userPortraitRulesMapper.selectAllEnableRules();
+    if (null != userPortraitRulesDoList && 0 < userPortraitRulesDoList.size()) {
+      for (UserPortraitRulesDo userPortraitRulesDo : userPortraitRulesDoList) {
+        String ruleName = userPortraitRulesDo.getRuleName();
+        if (ruleName.equals(ConstantsCode.USER_PORTRAIT_RULE_VISITED_TIME.getCode())) {
+          log.info(" # LoadUserPortraitFromDb.getAllUserPortraitRules() # 项目启动，从数据库中加载【{}】规则到内存里来。", ConstantsCode.USER_PORTRAIT_RULE_VISITED_TIME.getDesc());
+          initUserPortraitByVisitedTimeMap();
+        } else if (ruleName.equals(ConstantsCode.USER_PORTRAIT_RULE_VISITED_TABLE.getCode())) {
+          log.info(" # LoadUserPortraitFromDb.getAllUserPortraitRules() # 项目启动，从数据库中加载【{}】规则到内存里来。", ConstantsCode.USER_PORTRAIT_RULE_VISITED_TABLE.getDesc());
+          initUserPortraitByVisitedTableEverydayMap();
+        }
+      }
+    }
   }
 
   /**
@@ -69,7 +86,9 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
    * @Param []
    **/
   public Boolean initUserPortraitByVisitedTableEverydayMap() {
-    Map<String/* 用户名 */, Map<String/* 访问过的表 */, Map<String/* 访问日期，以天为单位 */,Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>>>> userPortraitByVisitedTableEverydayMap =
+    AnomylyDetectionSingletonByVisitedTableEveryday.setUserPortraitByVisitedTableEnable(true);
+
+    Map<String/* 用户名 */, Map<String/* 访问过的表 */, Map<String/* 访问日期，以天为单位 */, Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>>>> userPortraitByVisitedTableEverydayMap =
       AnomylyDetectionSingletonByVisitedTableEveryday.getUserPortraitByVisitedTableMap();
     Instant now = Instant.now();
     List<UserPortraitByVisitedTableEverydayDo> list = userPortraitByVisitedTableEverydayMapper.selectAllEnable();
@@ -88,20 +107,20 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
       String strToDateToStr = DateTimeUtil.dateToStr(date, DateTimeUtil.DATEFORMAT_STR_002);
 
       String userName = userPortraitByVisitedTableEverydayDo.getUserName();
-      Map<String/* 访问过的表 */, Map<String/* 访问日期，以天为单位 */,Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>>> visitedTableDateCountMap = userPortraitByVisitedTableEverydayMap.get(userName);
-      if(null == visitedTableDateCountMap){
+      Map<String/* 访问过的表 */, Map<String/* 访问日期，以天为单位 */, Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>>> visitedTableDateCountMap = userPortraitByVisitedTableEverydayMap.get(userName);
+      if (null == visitedTableDateCountMap) {
         visitedTableDateCountMap = new ConcurrentHashMap<>();
-        userPortraitByVisitedTableEverydayMap.put(userName,visitedTableDateCountMap);
+        userPortraitByVisitedTableEverydayMap.put(userName, visitedTableDateCountMap);
       }
-      Map<String/* 访问日期，以天为单位 */,Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>> dateDbTypeCountMap = visitedTableDateCountMap.get(tableName);
+      Map<String/* 访问日期，以天为单位 */, Map<String,/* 数据库操作类型：insert、delete、update、select */ Integer/* 访问次数 */>> dateDbTypeCountMap = visitedTableDateCountMap.get(tableName);
       if (null == dateDbTypeCountMap) {
         dateDbTypeCountMap = new ConcurrentHashMap<>();
         visitedTableDateCountMap.put(tableName, dateDbTypeCountMap);
       }
       Map<String, Integer> dbTypeCountMap = dateDbTypeCountMap.get(strToDateToStr);
-      if(null == dbTypeCountMap){
+      if (null == dbTypeCountMap) {
         dbTypeCountMap = new ConcurrentHashMap<>();
-        dateDbTypeCountMap.put(strToDateToStr,dbTypeCountMap);
+        dateDbTypeCountMap.put(strToDateToStr, dbTypeCountMap);
       }
       if (null != visitedCount && StringUtil.isNotBlank(dbType)) {
         dbTypeCountMap.put(dbType, visitedCount);
@@ -121,6 +140,8 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
    * @Param []
    **/
   public Boolean initUserPortraitByVisitedTimeMap() {
+    // 设置启用标志；2022-06-23 16:08:25
+    AnomylyDetectionSingletonByVisitedTime.setUserPortraitByVisitedTimeEnable(true);
     Map<String, Map<String, Integer>> userPortraitByVisitedTimeMap = AnomylyDetectionSingletonByVisitedTime.getUserPortraitByVisitedTimeMap();
     Instant now = Instant.now();
     List<UserPortraitByVisitedTimeDo> list = userPortraitByVisitedTimeMapper.selectAllEnable();
@@ -142,13 +163,13 @@ public class LoadUserPortraitFromDb implements ApplicationRunner {
         userPortraitByVisitedTimeMap.put(userName, map);
       }
       if (null != forenoonCount) {
-        map.put(ConstantsCode.USER_PORTRAIT_FORENOON.getMsgEn(), forenoonCount);
+        map.put(ConstantsCode.USER_PORTRAIT_FORENOON.getCode(), forenoonCount);
       }
       if (null != afternoonCount) {
-        map.put(ConstantsCode.USER_PORTRAIT_AFTERNOON.getMsgEn(), afternoonCount);
+        map.put(ConstantsCode.USER_PORTRAIT_AFTERNOON.getCode(), afternoonCount);
       }
       if (null != nightCount) {
-        map.put(ConstantsCode.USER_PORTRAIT_NIGHT.getMsgEn(), nightCount);
+        map.put(ConstantsCode.USER_PORTRAIT_NIGHT.getCode(), nightCount);
       }
     }
     log.info("# LoadUserPortraitFromDb.initUserPortraitByVisitedTimeMap() # 项目启动完毕，从数据库中查询到用户的基于访问时间的画像信息【{}条】，用时【{}】毫秒。", userPortraitByVisitedTimeMap.size(), DateTimeUtil.getTimeMillis(now));
