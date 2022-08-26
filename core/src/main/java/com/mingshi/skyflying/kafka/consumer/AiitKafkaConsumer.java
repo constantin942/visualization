67333ea3ff369c3f37cd0df2d667407bf8566be3
zhaoming <yiming2008@126.com>
2022-08-26 14:17:23 +0800

    更新配置文件；

--- core/src/main/java/com/mingshi/skyflying/kafka/consumer/AiitKafkaConsumer.java
+++ core/src/main/java/com/mingshi/skyflying/kafka/consumer/AiitKafkaConsumer.java
@@ -1,10 +1,18 @@
 // package com.mingshi.skyflying.kafka.consumer;
 //
+// import com.mingshi.skyflying.test.TestProcesserAndIoThreaderList;
+// import com.mingshi.skyflying.test.domain.TestRecordAckDo;
+// import com.mingshi.skyflying.test.thread.TestProcessorThread;
 // import lombok.extern.slf4j.Slf4j;
+// import org.apache.kafka.clients.consumer.ConsumerRecord;
+// import org.apache.kafka.common.utils.Bytes;
 // import org.springframework.context.annotation.PropertySource;
+// import org.springframework.kafka.annotation.KafkaListener;
+// import org.springframework.kafka.support.Acknowledgment;
 // import org.springframework.stereotype.Component;
 //
 // import javax.annotation.Resource;
+// import java.util.*;
 //
 // @Component
 // @Slf4j
@@ -13,6 +21,49 @@
 //   @Resource
 //   private AiitKafkaConsumerUtil aiitKafkaConsumerUtil;
 //
+//   private List<String> list = new ArrayList<>();
+//
+//   @KafkaListener(topics = "zm-test-topic-01", groupId = "zm-test-consumer-group")
+//   public void onMessage(ConsumerRecord<String, Bytes> record, Acknowledgment ack) {
+//     Optional message = Optional.ofNullable(record.value());
+//     if (message.isPresent()) {
+//       TestRecordAckDo testRecordAckDo = new TestRecordAckDo();
+//       testRecordAckDo.setRecord(record);
+//       testRecordAckDo.setAck(ack);
+//
+//       Integer partition = record.partition();
+//       TestProcessorThread testProcessorThread = TestProcesserAndIoThreaderList.getTestProcessorThread(partition);
+//       testProcessorThread.offer(testRecordAckDo);
+//
+//       long offset = record.offset();
+//
+//       String key = record.topic() + "#" + partition + "#" + offset;
+//       if (list.contains(key)) {
+//         log.error("当前线程 = {} 已包含 当前的offset = {}。", Thread.currentThread().getName(), key);
+//       }else{
+//         list.add(key);
+//       }
+//
+//       // String topic = record.topic();
+//       // Bytes value = record.value();
+//       // Map<Integer, List<Long>> integerLongMap = map.get(topic);
+//       // if(null == integerLongMap){
+//       //   integerLongMap = new HashMap<>();
+//       //   map.put(topic,integerLongMap);
+//       // }
+//       // List<Long> offsetList = integerLongMap.get(partition);
+//       // if(null == offsetList){
+//       //   offsetList = new LinkedList<>();
+//       //   integerLongMap.put(partition,offsetList);
+//       // }
+//       // if(offsetList.contains(offset)){
+//       //   log.error("");
+//       // }else{
+//       //   offsetList.add(offset);
+//       // }
+//     }
+//   }
+//   //
 //   // @KafkaListener(topics = "skywalking-segments", groupId = "skyflying-consumer-group")
 //   // public void onMessage(ConsumerRecord<String, Bytes> record, Acknowledgment ack) {
 //   //   Optional message = Optional.ofNullable(record.value());
