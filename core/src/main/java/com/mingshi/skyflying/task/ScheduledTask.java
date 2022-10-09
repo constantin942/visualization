efb07ad32653db31918a07bfeb3f70d0a0b3f652
zhaoming <yiming2008@126.com>
2022-10-09 09:23:20 +0800

    去掉没用的代码；

--- core/src/main/java/com/mingshi/skyflying/task/ScheduledTask.java
+++ core/src/main/java/com/mingshi/skyflying/task/ScheduledTask.java
@@ -75,43 +75,6 @@ public class ScheduledTask {
   public void scheduledUpdateUserNameByGlobalTraceId() {
     execitonScheduledTaskList.doScheduledUpdateUserNameByGlobalTraceId(Const.SCHEDULED_UPDATE_USER_NAME_BY_GLOBAL_TRACEID);
   }
-//
-//  /**
-//   * <B>方法名称：scheduledUpdateUserPortraitByVisitedTime</B>
-//   * <B>概要说明：定时更新基于访问时间的用户画像信息</B>
-//   * 只有用户画像信息有变更了，才将其更新到数据库中。其目的是减少数据库的访问压力。
-//   *
-//   * @return void
-//   * @Author zm
-//   * @Date 2022年06月08日 10:06:01
-//   * @Param []
-//   **/
-//  /**
-//   * 每隔30分钟执行一次：
-//   */
-//  // @Scheduled(cron = "0 */1 * * * ?")
-//  @Scheduled(cron = "0 0 0/3 * * ?")
-//  public void scheduledUpdateUserPortrait() {
-//    execitonScheduledTaskList.doScheduledUpdateUserPortrait(Const.SCHEDULED_UPDATE_USER_PORTRAIT);
-//  }
-
-//  /**
-//   * <B>方法名称：scheduledGetNoCheckAbnormalRecord</B>
-//   * <B>概要说明：定时将数据库中用户名不为空，且未进行过基于访问时间的异常检测记录查询出来，然后进行异常检测</B>
-//   *
-//   * @return void
-//   * @Author zm
-//   * @Date 2022年06月08日 09:06:58
-//   * @Param []
-//   **/
-//  /**
-//   * 每隔30分钟执行一次：
-//   */
-//  // @Scheduled(cron = "0 */1 * * * ?")
-//  @Scheduled(cron = "0 0 0/4 * * ?")
-//  public void scheduledGetNoCheckAbnormalRecord() {
-//    execitonScheduledTaskList.doScheduledGetNoCheckAbnormalRecord(Const.SCHEDULED_GET_NO_CHECK_ABNORMAL_RECORD);
-//  }
 
   /**
    * <B>方法名称：scheduledGetDmsAuditLog</B>
@@ -125,6 +88,7 @@ public class ScheduledTask {
   /**
    * 每隔30分钟执行一次：
    */
+  @Scheduled(cron = "0 */1 * * * ?")
   // @Scheduled(cron = "0 */30 * * * ?")
   public void scheduledGetDmsAuditLog() {
     execitonScheduledTaskList.doScheduledGetDmsAuditLog(Const.SCHEDULED_GET_DMS_AUDIT_LOG);
