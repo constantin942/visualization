bf02a54336bba01bb6c8ea6c1b82f4871c9b20bd
zhaoming <yiming2008@126.com>
2022-10-31 09:43:37 +0800

    识别哪些定时任务，然后将数据库的用户名当做登录系统的用户名；

--- core/src/main/java/com/mingshi/skyflying/impl/SegmentConsumeServiceImpl.java
+++ core/src/main/java/com/mingshi/skyflying/impl/SegmentConsumeServiceImpl.java
@@ -267,7 +267,7 @@ public class SegmentConsumeServiceImpl implements SegmentConsumerService {
                             } else if (Const.DB_INSTANCE.equals(key)) {
                                 msSegmentDetailDo.setDbInstance(value);
                             } else if (Const.DB_USER_NAME.equals(key)) {
-                                msSegmentDetailDo.setDbUserName(value);
+                                setUserName(msSegmentDetailDo,value);
                             } else if (Const.DB_STATEMENT.equals(key)) {
                                 if (Const.OPERATION_TYPE_SQL.equals(isSql)) {
                                     if ((StringUtil.isNotBlank(logs) && !logs.equals(Const.IS_NULL)) || StringUtil.isBlank(value)) {
@@ -315,6 +315,26 @@ public class SegmentConsumeServiceImpl implements SegmentConsumerService {
             log.error("# SegmentConsumeServiceImpl.getSegmentDetaiDolList() # 组装segmentDetail详情实例时，出现了异常。", e);
         }
         return segmentDetaiDolList;
+    }
+
+    /**
+     * <B>方法名称：setUserName</B>
+     * <B>概要说明：设置用户名</B>
+     *
+     * @Author zm
+     * @Date 2022-10-28 17:33:46
+     * @Param [msSegmentDetailDo, value]
+     * @return void
+     **/
+    private void setUserName(MsSegmentDetailDo msSegmentDetailDo, String value) {
+        msSegmentDetailDo.setDbUserName(value);
+        String operationName = msSegmentDetailDo.getOperationName();
+        String parentEndpoint = msSegmentDetailDo.getParentEndpoint();
+        String userName = msSegmentDetailDo.getUserName();
+        // 如果是来自spring框架提供的定时任务，那么用户名是空的。此时把访问数据库的用户名（dbUserName）当做登录系统的用户名（userName）；2022-10-28 17:11:23
+        if(StringUtil.isBlank(userName) && (operationName.contains(Const.SPRING_SCHEDULED) || operationName.startsWith(Const.SPRING_SCHEDULED) || parentEndpoint.contains(Const.SPRING_SCHEDULED) || operationName.startsWith(Const.SPRING_SCHEDULED))){
+            msSegmentDetailDo.setUserName(value);
+        }
     }
 
     /**
@@ -484,7 +504,7 @@ public class SegmentConsumeServiceImpl implements SegmentConsumerService {
      * @Param [operationName]
      **/
     private Boolean ignoreMethod(String operationName) {
-        if (Const.OPERATION_NAME_MAP.containsKey(operationName) || operationName.startsWith(Const.SPRING_SCHEDULED)) {
+        if (Const.OPERATION_NAME_MAP.containsKey(operationName)) {
             return true;
         }
         return false;
